fc8b855054d37133975e1d81ee008c4f2d3e2417
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-10-31 07:42:47 -0700

    Added more tests and functionality for addBook(). Also add .gitignore for IntelliJ IDEA.

--- src/Library.java
+++ src/Library.java
@@ -2,22 +2,33 @@ import java.util.ArrayList;
 
 public class Library {
     private final int A_NUM = 7; // number of attributes for each books
-    public ArrayList<String[]> books; // books list
+    ArrayList<String[]> books; // books list
 
     Library() {
         books = new ArrayList<>();
     }
-    void addBook(String T, String A, String G, String Y, String P){
+
+    void addBook(String title, String author, String genre, String year, String pageNum) {
+        // test year and page number are numeric
+        try {
+            Double.parseDouble(year);
+            Double.parseDouble(pageNum);
+        }
+        catch(NumberFormatException e) {
+            throw new StringNotNumberException("Year or page number is not numeric");
+        }
+
+        // add a new book to the array
         books.add(new String[A_NUM]);
-        String[] book = books.get(books.size() - 1);
 
-        book[0] = String.valueOf(books.size() - 1);
-        book[1] = "In";
-        book[2] = T;
-        book[3] = A;
-        book[4] = G;
-        book[5] = Y;
-        book[6] = P;
+        // set book properties
+        String[] book = books.get(books.size() - 1);  // reference to sepecific book
+        book[0] = String.valueOf(books.size() - 1);  // book ID is the index num
+        book[1] = "In";  // books are checked in by default
+        book[2] = title;
+        book[3] = author;
+        book[4] = genre;
+        book[5] = year;
+        book[6] = pageNum;
     }
-
 }
---
+++ src/StringNotNumberException.java
@@ -0,0 +1,5 @@
+public class StringNotNumberException extends RuntimeException {
+    public StringNotNumberException(String message) {
+        super(message);
+    }
+}
