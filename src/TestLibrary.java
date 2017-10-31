bb27b7638277aec3e561a1519200f317a8852621
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-10-30 19:46:53 -0700

    Initial commit, added classes with stubbed out tests and a documentation file.

---
+++ src/TestLibrary.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestLibrary {
+    @Test public void addBookTest() {
+
+    }
+
+    @Test public void queryBookTest() {
+
+    }
+
+    @Test public void editBookTest() {
+
+    }
+
+    @Test public void removeBookTest() {
+
+    }
+}
