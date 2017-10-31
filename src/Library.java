a9ee094d0b87bb843d5a8d6b7509ac8e7a111d3d
Nick Perry <perrni@csl02.cs.wallawalla.edu>
2017-10-30 21:28:30 -0700

    Added testing and functionality for the addBook() method.

--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
 
 public class Library {
-    private final int A_NUM = 7; // number of attributes for each book
-    private ArrayList<String[]> book; // book list
+    private final int A_NUM = 7; // number of attributes for each books
+    public ArrayList<String[]> books; // books list
 
     Library() {
-        book = new ArrayList<>();
+        books = new ArrayList<>();
+    }
+    void addBook(String T, String A, String G, String Y, String P){
+        books.add(new String[A_NUM]);
+        String[] book = books.get(books.size() - 1);
+
+        book[0] = String.valueOf(books.size() - 1);
+        book[1] = "In";
+        book[2] = T;
+        book[3] = A;
+        book[4] = G;
+        book[5] = Y;
+        book[6] = P;
     }
 
 }
--- src/TestLibrary.java
+++ src/TestLibrary.java
@@ -1,10 +1,20 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestLibrary {
-    @Test public void addBookTest() {
+    private Library lib;
+    @Before public void setUp() {
+        lib = new Library();
 
+    }
+
+
+    @Test public void addBookTest() {
+        String[] bookArr = {"0", "In", "Moby Dick", "That Man", "Adventure", "1969", "200"};
+        lib.addBook(bookArr[2], bookArr[3], bookArr[4], bookArr[5], bookArr[6]);
+        Assert.assertArrayEquals(bookArr, lib.books.get(0));
     }
 
     @Test public void queryBookTest() {
